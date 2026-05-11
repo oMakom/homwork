6c0cc7e39c24c271ee66c6b61d25f1c3b7ec5dff
Your Name <you@example.com>
2026-05-11 20:47:04 +0300

    Доработка функций модуля processing.py

--- scr/processing.py
+++ scr/processing.py
@@ -1,5 +1,4 @@
-
-def filter_by_state(transactions_list: list, state: str='EXECUTED') -> list:
+def filter_by_state(transactions_list: list, state: str = "EXECUTED") -> list:
     """
     Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
     Возвращает новый список словарей, содержащий только те словари, у которых state соответствует указанному значению
@@ -7,16 +6,17 @@ def filter_by_state(transactions_list: list, state: str='EXECUTED') -> list:
     the_filtered_list = []
 
     for transactions in transactions_list:
-        if transactions['state'] == state:
+        if transactions["state"] == state:
             the_filtered_list.append(transactions)
 
     return the_filtered_list
 
-def sort_by_date(transactions_list: list, is_reverse: bool=True) -> list:
+
+def sort_by_date(transactions_list: list, is_reverse: bool = True) -> list:
     """
     Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
     Функция должна возвращать новый список, отсортированный по дате
     """
-    the_filtered_list = sorted(transactions_list, key=lambda key_date: key_date['date'], reverse=is_reverse)
+    the_sorted_list = sorted(transactions_list, key=lambda key_date: key_date["date"], reverse=is_reverse)
 
-    return the_filtered_list
+    return the_sorted_list
