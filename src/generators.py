8cf578ec4eecbd1e5c40f6b2a1123fcdb3a74ab1
Your Name <you@example.com>
2026-05-23 11:35:19 +0300

    fix src.generators и add test для filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -2,6 +2,10 @@ from typing import Iterator
 
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
+    """
+    Принимает на вход список словарей, представляющих транзакции и валюту операции
+    Возвращает итератор, который поочередно выдает транзакции соответствующие заданной валюте операции
+    """
     for transaction in transactions:
         if transaction.get("operationAmount").get("currency").get("code") == currency:
             yield transaction
@@ -10,6 +14,10 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict
 
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
+    """
+    Принимает список словарей с транзакциями
+    Возвращает описание каждой операции по очереди
+    """
     for transaction in transactions:
         if transaction.get("description"):
             yield transaction["description"]
@@ -18,17 +26,22 @@ def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
 
 
 def card_number_generator(start: int, end: int) -> Iterator[str]:
+    """
+    Генератор который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX
+    Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999
+    """
     if start > end:
         dubbl_start = start
         start = end
         end = dubbl_start
 
-    for card_number in range(start, end+1):
-        str_card_number = str(card_number)
-        if len(str_card_number) > 16:
+    for card_number in range(start, end + 1):
+        if card_number > 9999999999999999:
             break
-        while len(str_card_number) < 16:
-            str_card_number = "0" + str_card_number
 
-        result_card_number = f"{str_card_number[0:4]} {str(str_card_number)[4:8]} {str(str_card_number)[8:12]} {str(str_card_number)[12:16]}"
+        str_card_number = f"{card_number:016d}"
+        result_card_number = (
+            f"{str_card_number[0:4]} {str(str_card_number)[4:8]} {str(str_card_number)[8:12]}"
+            f" {str(str_card_number)[12:16]}"
+        )
         yield result_card_number
