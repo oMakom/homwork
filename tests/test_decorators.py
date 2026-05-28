6a246543bbbcdab5016f4ee7bb36a3ed61279bf7
Your Name <you@example.com>
2026-05-28 21:17:43 +0300

    Реализован  тест log decorator и выведен отчет тестов

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,1 +1,20 @@
-import pytest
+from src.decorators import log
+
+
+def test_log_to_console(capsys):
+    @log()
+    def test_func(x, y):
+        return x / y + x
+
+    test_func(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out == "test_func ok\n"
+    test_func(3, 0)
+    captured = capsys.readouterr()
+    assert captured.out == "test_func error: division by zero. Inputs: args=(3, 0), kwargs={}\n"
+    test_func("3", 0)
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == "test_func error: unsupported operand type(s) for /: 'str' and 'int'. Inputs: args=('3', 0), kwargs={}\n"
+    )
