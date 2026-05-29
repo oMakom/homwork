c40320bab1ff97dd1b2d608f4e171932512f82b2
Your Name <you@example.com>
2026-05-29 22:15:40 +0300

    Fix log decorator

--- src/decorators.py
+++ src/decorators.py
@@ -3,6 +3,11 @@ from typing import Any, Optional
 
 
 def log(filename: Optional[str] = None):
+    """
+    Декоратор: логирует  начало и конец выполнения функции, ее результаты или возникшие ошибки.
+    Принимает необязательный аргумент 'filename', который определяет,
+    куда будут записываться логи (в файл или в консоль)
+    """
     def log_decorator(function: Any):
         @wraps(function)
         def wrapper(*args: Any, **kwargs: Any):
@@ -15,6 +20,7 @@ def log(filename: Optional[str] = None):
                         log_file.write(log_message + "\n")
                 else:
                     print(log_message)
+                return try_result
             except Exception as error:
                 error_log_message = f"{name_func} error: {error}. Inputs: args={args}, kwargs={kwargs}"
                 if filename:
