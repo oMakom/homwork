617b0d0097c3e7c2c0159769a54e8cf17a6b0add
Your Name <you@example.com>
2026-06-06 10:10:37 +0300

    fix exchange_rate

--- src/external_api.py
+++ src/external_api.py
@@ -7,9 +7,9 @@ def exchange_rate(currency: str = "RUB") -> float:
     Получает код курса валют, на выходе курс валюты по отношению к рублю
     """
     url = "https://www.cbr-xml-daily.ru/daily_json.js"
-    list_of_currencies = requests.get(url).json()
     if currency == "RUB":
         return 1.0
+    list_of_currencies = requests.get(url).json()
     try:
         result = list_of_currencies.get("Valute").get(currency).get("Value")
     except:
