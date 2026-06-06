6fc36c64b9cc910648c6b4e40a89fe0b1bd4ac3f
Your Name <you@example.com>
2026-06-06 15:36:09 +0300

    Fix test_external_api

--- src/external_api.py
+++ src/external_api.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 import requests
 
 
@@ -6,12 +8,13 @@ def exchange_rate(currency: str = "RUB") -> float:
     Обращение к внешнему источнику для получения текущего курса валют и конвертации суммы операции в рубли
     Получает код курса валют, на выходе курс валюты по отношению к рублю
     """
-    url = "https://www.cbr-xml-daily.ru/daily_json.js"
-    if currency == "RUB":
-        return 1.0
-    list_of_currencies = requests.get(url).json()
-    try:
-        result = list_of_currencies.get("Valute").get(currency).get("Value")
-    except:
-        return 1.0
+    result = 1.0
+    if currency == "USD" or currency == "EUR":
+        load_dotenv()
+        Api_Key = os.getenv("API_KEY")
+        list_of_currencies = requests.get(f"https://currate.ru/api/?get=rates&pairs={currency}RUB&key={Api_Key}").json()
+        try:
+            result = list_of_currencies.get("data").get(currency+"RUB")
+        except:
+            return 1.0
     return result
