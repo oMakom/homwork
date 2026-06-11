0b3da0b81d582342a31423bb529cc977df5488cc
Your Name <you@example.com>
2026-06-11 13:27:55 +0300

    Fix masks.py и utilspy

--- src/utils.py
+++ src/utils.py
@@ -3,13 +3,13 @@ import logging
 
 import src.external_api as external_api
 
-
 logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("logs/utils.log", mode ='w', encoding="utf-8")
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler("logs/utils.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
+
 
 def transaction_json_in_python(json_path: str = "data/operations.json") -> list | list[dict]:
     """
@@ -17,18 +17,18 @@ def transaction_json_in_python(json_path: str = "data/operations.json") -> list 
     Путь по умолчанию 'data/operations.json'
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список
     """
-    logger.info(f'Вызов transaction_json_in_python с путем до JSON-файла({json_path})')
+    logger.info(f"Вызов transaction_json_in_python с путем до JSON-файла({json_path})")
     try:
-        logger.info(f'transaction_json_in_python попытка открытия файла транзакций')
+        logger.info("transaction_json_in_python попытка открытия файла транзакций")
         with open(json_path, encoding="utf-8") as data_file:
             transaction_data = json.load(data_file)
             if not isinstance(transaction_data, list):
-                logger.error(f'transaction_json_in_python данные не список, отдаем пустой список')
+                logger.error("transaction_json_in_python данные не список, отдаем пустой список")
                 transaction_data = []
     except (FileNotFoundError, PermissionError, UnicodeDecodeError, json.JSONDecodeError) as e:
-        logger.error(f'transaction_json_in_python ошибка {e} ; отдаем пустой список')
+        logger.error(f"transaction_json_in_python ошибка {e} ; отдаем пустой список")
         transaction_data = []
-    logger.info(f'transaction_json_in_python чтение файла транзакций завершено')
+    logger.info("transaction_json_in_python чтение файла транзакций завершено")
     return transaction_data
 
 
@@ -38,15 +38,15 @@ def transaction_amount(transaction: dict) -> float:
     Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения
     текущего курса валют и конвертации суммы операции в рубли
     """
-    logger.info(f'Вызов transaction_amount)')
+    logger.info("Вызов функции transaction_amount")
     if transaction.get("operationAmount").get("currency").get("code") and transaction.get("operationAmount").get(
         "amount"
     ):
         operation_code = transaction.get("operationAmount").get("currency").get("code")
         operation_amount = transaction.get("operationAmount").get("amount")
         result_value = round(float(operation_amount) * float(external_api.exchange_rate(operation_code)), 2)
-        logger.info(f'transaction_amount успешное выполнение функции)')
+        logger.info("transaction_amount успешное выполнение функции")
         return result_value
     else:
-        logger.error(f'transaction_amount ошибка выполнения, сумма транзакций будет равна 0)')
+        logger.error("transaction_amount ошибка выполнения, сумма транзакций будет равна 0")
         return 0
