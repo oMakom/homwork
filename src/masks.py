f3b9f0b94e88219bcd39cd37467043514bb8ee2b
Your Name <you@example.com>
2026-06-11 12:53:39 +0300

    Реализация логов для masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,26 @@
+import logging
+import datetime
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/masks.log", mode ='w', encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 def get_mask_card_number(number_card: int | str) -> str:
     """
     принимает на вход номер карты и возвращает ее маску
     XXXX XX** **** XXXX
     """
+    logger.info(f'get_mask_card_number принимает номер карты: {number_card}')
     mask_card = ""
     counter = 1
     without_spaces_number_card = str(number_card).replace(" ", "")
 
     for num in str(without_spaces_number_card):
         if num.isalpha():
+            logger.error(f'get_mask_card_number ошибка "Буквенный ввод недопустим". Завершение функции')
             return "Буквенный ввод недопустим"
         if num == " ":
             continue
@@ -19,7 +31,7 @@ def get_mask_card_number(number_card: int | str) -> str:
         if counter % 4 == 0 and counter < len(str(without_spaces_number_card)):
             mask_card += " "
         counter += 1
-
+    logger.info(f'get_mask_card_number завершение обработки номера карты')
     return mask_card
 
 
@@ -28,17 +40,19 @@ def get_mask_account(account_number: int | str) -> str:
     принимает на вход номер счета и возвращает его маску
     **XXXX
     """
+    logger.info(f'get_mask_account принимает номер счета: {account_number}')
     mask_account = ""
     counter = 1
     without_spaces_account_number = str(account_number).replace(" ", "")
 
     for num in str(without_spaces_account_number):
         if num.isalpha():
+            logger.error(f'get_mask_account ошибка "Буквенный ввод недопустим". Завершение функции')
             return "Буквенный ввод недопустим"
         if counter > len(str(without_spaces_account_number)) - 4:
             mask_account += str(num)
         elif counter > len(str(without_spaces_account_number)) - 6:
             mask_account += "*"
         counter += 1
-
+    logger.info(f'get_mask_account завершение обработки номера счета')
     return mask_account
