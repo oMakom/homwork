daf49201ee999979c11434ad8abff0094f8af52e
Your Name <you@example.com>
2026-06-12 11:37:38 +0300

    реализация функции read_cvs_file с логами

---
+++ src/read_files.py
@@ -0,0 +1,39 @@
+import csv
+import logging
+import pandas as pd
+from typing import Any, Dict, List, Union
+
+
+logger = logging.getLogger("read_files")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/read_files.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+def read_cvs_file(file_path):
+    logger.info(f"вызов read_cvs_file с путем до файла: {file_path}")
+    transactions: List[Dict[str, Any]] = []
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            logger.info(f"read_cvs_file чтение файла: {file_path}")
+            read_cvs = csv.DictReader(f, delimiter=";")
+            for row in read_cvs:
+                if any(value for value in row.values() if value):
+                    transactions.append(dict(row))
+    except  FileNotFoundError as er:
+        logger.error(f"read_cvs_file ошибка открытия файла: {er}")
+    except:
+        logger.error(f"read_cvs_file непредвиденная ошибка")
+    logger.info("read_cvs_file завершение работы")
+    return transactions
+
+
+def read_exel_file(file_path):
+    #try:
+    #    df = pd.read_excel(file_path)
+    #    transactions = df.to_dict()
+    #    return transactions
+    #except:
+    #    print("Ошибка Чтения")
+    pass
