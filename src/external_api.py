edc853c6b674e3af97877e8711d69a8e5f588165
Your Name <you@example.com>
2026-06-12 12:19:13 +0300

    add logs for external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -1,7 +1,15 @@
 import os
-
+import logging
 import requests
 from dotenv import load_dotenv
+
+
+logger = logging.getLogger("external_api.py")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/external_api.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def exchange_rate(currency: str = "RUB") -> float:
@@ -9,6 +17,7 @@ def exchange_rate(currency: str = "RUB") -> float:
     Обращение к внешнему источнику для получения текущего курса валют и конвертации суммы операции в рубли
     Получает код курса валют, на выходе курс валюты по отношению к рублю
     """
+    logger.info(f"вызов exchange_rate для валюты : {currency}")
     result = 1.0
     if currency == "USD" or currency == "EUR":
         load_dotenv()
@@ -18,6 +27,8 @@ def exchange_rate(currency: str = "RUB") -> float:
         ).json()
         try:
             result = list_of_currencies.get("data").get(currency + "RUB")
-        except:
+            logger.info(f"exchange_rate полученние данных по курсу валют({currency}: {result})")
+        except Exception as e:
+            logger.error(f"exchange_rate ошибка получение курса по {currency}: {e})")
             return 1.0
     return result
