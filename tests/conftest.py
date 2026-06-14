04a3f49220bac03ccf3bdafaaa209828ccd656fc
Your Name <you@example.com>
2026-06-14 19:52:38 +0300

    создание @pytest.fixture data_operations_dict()

---
+++ tests/conftest.py
@@ -0,0 +1,134 @@
+import pytest
+
+@pytest.fixture
+def data_operations_dict():
+    return [
+  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  },
+  {
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+  {
+    "id": 587085106,
+    "state": "EXECUTED",
+    "date": "2018-03-23T10:45:06.972075",
+    "operationAmount": {
+      "amount": "48223.05",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 41421565395219882431"
+  },
+  {
+    "id": 142264268,
+    "state": "EXECUTED",
+    "date": "2019-04-04T23:20:05.206878",
+    "operationAmount": {
+      "amount": "79114.93",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод со счета на счет",
+    "from": "Счет 19708645243227258542",
+    "to": "Счет 75651667383060284188"
+  },
+  {
+    "id": 873106923,
+    "state": "EXECUTED",
+    "date": "2019-03-23T01:09:46.296404",
+    "operationAmount": {
+      "amount": "43318.34",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод со счета на счет",
+    "from": "Счет 44812258784861134719",
+    "to": "Счет 74489636417521191160"
+  },
+  {
+    "id": 716496732,
+    "state": "EXECUTED",
+    "date": "2018-04-04T17:33:34.701093",
+    "operationAmount": {
+      "amount": "40701.91",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Visa Gold 5999414228426353",
+    "to": "Счет 72731966109147704472"
+  },
+  {
+    "id": 518707726,
+    "state": "EXECUTED",
+    "date": "2018-11-29T07:18:23.941293",
+    "operationAmount": {
+      "amount": "3348.98",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод с карты на карту",
+    "from": "MasterCard 3152479541115065",
+    "to": "Visa Gold 9447344650495960"
+  },
+  {
+    "id": 743628025,
+    "state": "EXECUTED",
+    "date": "2018-06-04T06:59:55.424356",
+    "operationAmount": {
+      "amount": "978.31",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "from": "Счет 54883981902864782073",
+    "to": "Счет 61834060137088759145"
+  },
+  {
+    "id": 743278119,
+    "state": "EXECUTED",
+    "date": "2018-10-15T08:05:34.061711",
+    "operationAmount": {
+      "amount": "51203.12",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "",
+    "from": "MasterCard 1435442169918409",
+    "to": "Maestro 7452400219469235"
+  },]
