0f87610d81ee7321fd26f2b707147432faa7cdfb
Your Name <you@example.com>
2026-06-14 20:27:28 +0300

    доработка @pytest.fixture data_operations_dict()

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,17 +19,6 @@ def data_operations_dict():
     "to": "Счет 64686473678894779589"
   },
   {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-  {
     "id": 587085106,
     "state": "EXECUTED",
     "date": "2018-03-23T10:45:06.972075",
@@ -57,51 +46,6 @@ def data_operations_dict():
     "description": "Перевод со счета на счет",
     "from": "Счет 19708645243227258542",
     "to": "Счет 75651667383060284188"
-  },
-  {
-    "id": 873106923,
-    "state": "EXECUTED",
-    "date": "2019-03-23T01:09:46.296404",
-    "operationAmount": {
-      "amount": "43318.34",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод со счета на счет",
-    "from": "Счет 44812258784861134719",
-    "to": "Счет 74489636417521191160"
-  },
-  {
-    "id": 716496732,
-    "state": "EXECUTED",
-    "date": "2018-04-04T17:33:34.701093",
-    "operationAmount": {
-      "amount": "40701.91",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Visa Gold 5999414228426353",
-    "to": "Счет 72731966109147704472"
-  },
-  {
-    "id": 518707726,
-    "state": "EXECUTED",
-    "date": "2018-11-29T07:18:23.941293",
-    "operationAmount": {
-      "amount": "3348.98",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод с карты на карту",
-    "from": "MasterCard 3152479541115065",
-    "to": "Visa Gold 9447344650495960"
   },
   {
     "id": 743628025,
