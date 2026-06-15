623b38ea96facb6010324ac57004868a9036a34f
Your Name <you@example.com>
2026-06-15 16:13:36 +0300

    Приведение кода к стандартам PEP8

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 import re
 from collections import defaultdict
 
+
 def filter_by_state(transactions_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
@@ -28,15 +29,17 @@ def sort_by_date(transactions_list: list[dict], is_reverse: bool = True) -> list
             id_transactions = transactions["id"]
             datetime.fromisoformat(transactions["date"])
     except ValueError:
-        return (f"Ошибка ввода: Введите кооректные данные по дате в ISO виде (например: 2024-03-11T02:26:18.671407) в "
-                f"транзакции ID {id_transactions}")
+        return (
+            f"Ошибка ввода: Введите кооректные данные по дате в ISO виде (например: 2024-03-11T02:26:18.671407) в "
+            f"транзакции ID {id_transactions}"
+        )
 
     the_sorted_list = sorted(transactions_list, key=lambda key_date: key_date["date"], reverse=is_reverse)
 
     return the_sorted_list
 
 
-def process_bank_search(data:list[dict], search:str) -> list[dict]:
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
     """
     Принимать список словарей с данными о банковских операциях и строку поиска, а возвращать список словарей,
     у которых в описании есть данная строка
@@ -48,7 +51,7 @@ def process_bank_search(data:list[dict], search:str) -> list[dict]:
     return result
 
 
-def process_bank_operations(data:list[dict], categories:list) -> dict:
+def process_bank_operations(data: list[dict], categories: list) -> dict:
     """
     Принимает список словарей с данными о банковских операциях и список категорий операций, а возвращает словарь,
     в котором ключи — это названия категорий, а значения — это количество операций в каждой категории.
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.processing import filter_by_state, sort_by_date, process_bank_search, process_bank_operations
+from src.processing import filter_by_state, process_bank_operations, process_bank_search, sort_by_date
 
 
 @pytest.mark.parametrize(
@@ -128,39 +128,44 @@ def test_sort_by_date(transactions_list: list[dict], is_reverse: bool, expected:
     assert sort_by_date(transactions_list, is_reverse) == expected
 
 
-@pytest.mark.parametrize("search, excepted", [("Перевод орган", [{
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  }]),
-    ("рытие вклад",[{
-    "id": 587085106,
-    "state": "EXECUTED",
-    "date": "2018-03-23T10:45:06.972075",
-    "operationAmount": {
-      "amount": "48223.05",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Открытие вклада",
-    "to": "Счет 41421565395219882431"
-  }])])
+@pytest.mark.parametrize(
+    "search, excepted",
+    [
+        (
+            "Перевод орган",
+            [
+                {
+                    "id": 441945886,
+                    "state": "EXECUTED",
+                    "date": "2019-08-26T10:50:58.294041",
+                    "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+                    "description": "Перевод организации",
+                    "from": "Maestro 1596837868705199",
+                    "to": "Счет 64686473678894779589",
+                }
+            ],
+        ),
+        (
+            "рытие вклад",
+            [
+                {
+                    "id": 587085106,
+                    "state": "EXECUTED",
+                    "date": "2018-03-23T10:45:06.972075",
+                    "operationAmount": {"amount": "48223.05", "currency": {"name": "руб.", "code": "RUB"}},
+                    "description": "Открытие вклада",
+                    "to": "Счет 41421565395219882431",
+                }
+            ],
+        ),
+    ],
+)
 def test_process_bank_search(data_operations_dict, search, excepted) -> None:
     assert process_bank_search(data_operations_dict, search) == excepted
 
 
 def test_process_bank_operations(data_operations_dict) -> None:
     assert process_bank_operations(data_operations_dict, ["Перевод организации", "Открытие вклада"]) == {
-           "Перевод организации": 1, "Открытие вклада": 1}
+        "Перевод организации": 1,
+        "Открытие вклада": 1,
+    }
